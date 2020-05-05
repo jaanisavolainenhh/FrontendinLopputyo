869a625221a728d6c79adfd9f9ceb4bec53e9dff
Jaani Savolainen <bgh273@myy.haaga-helia.fi>
2020-05-05 13:00:14 +0300

    kalenteri eventit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+
 import CustomerList from './components/CustomerList';
 import TrainingList from './components/TrainingList';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Kalenteri from './components/Kalenteri'
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Simpletabs from './components/Ylapalkki'
+
+
 function App() {
+
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <div className="App">
       <AppBar position="static">
@@ -15,12 +49,14 @@ function App() {
             TRAINEEEER
           </Typography>
         </Toolbar>
+      
       </AppBar>
-      <Kalenteri />
-      <CustomerList />
-      <TrainingList />
+       <Simpletabs />
+       {/* <CustomerList /> */}
     </div>
   );
 }
 
+
 export default App;
+
--- src/components/Kalenteri.js
+++ src/components/Kalenteri.js
@@ -8,22 +8,69 @@ import './main.scss'
 
 export default function Kalenteri() {
 
-const [calendarWeekends, setCalendarWeekends] = React.useState(true)
-const [calendarEvents, setCalendarEvents] = React.useState([  { title: 'Event Now', start: new Date() }])
+    const [trainings, setTrainings] = React.useState([]);
+
+    const [calendarWeekends, setCalendarWeekends] = React.useState(true)
+    const [calendarEvents, setCalendarEvents] = React.useState([
+        { title: 'jee jee', start: '2020-05-04T16:20:15', end: '2020-05-04T21:20:15' },
+        { title: 'Testi', start: '2020-05-04T20:20:15', end: '2020-05-04T22:22:15' },
+        //{ title: 'Bla bla', start: '2020-05-04',end: '2020-05-05' }
+
+    ])
+
+    
+    React.useEffect(() => {
+        getTrainings();
+
+    }, [])
+
+    const getTrainings = () => {
+        fetch('https://customerrest.herokuapp.com/gettrainings')
+            .then(response => response.json())
+            .then(
+                data => {
+
+                    let datat = data;
+                    datat.map((daa, index) => {
+                        if (daa.customer != null)
+                            daa.customer.kokonimi = daa.customer.firstname + ' ' + daa.customer.lastname
+                    })
+                    // console.log(datat)
+                    //datat.map((daa))
+                    //setTrainings(datat)
+                    luoEventit(datat)
+                })
+            .catch(err => console.error(err))
+    }
+
+    function luoEventit(data)
+    {
+        let eventit = Array();
+        data.map((daa, index) => {
+            let uusiDate = new Date(daa.date);
+            uusiDate.setMinutes(uusiDate.getMinutes()+daa.duration)
+            //uusiDate.date
+            //uusiDate.setMinutes(daa.duration)
+            console.log(uusiDate)
+            eventit.push({title: daa.activity+ " / "+daa.customer.kokonimi, start: daa.date, end: uusiDate})
+        })
+        setCalendarEvents(eventit);
+
+    }
 
     let calendarComponentRef = React.createRef()
 
-   const  toggleWeekends = () => {
-        setCalendarWeekends(!calendarWeekends);
-        }
-    
+    //    const  toggleWeekends = () => {
+    //         setCalendarWeekends(!calendarWeekends);
+    //         }
 
-    const gotoPast = () => {
-        let calendarApi = this.calendarComponentRef.current.getApi()
-        calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
-    }
 
-   const handleDateClick = (arg) => {
+    //     const gotoPast = () => {
+    //         let calendarApi = this.calendarComponentRef.current.getApi()
+    //         calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
+    //     }
+
+    const handleDateClick = (arg) => {
         // if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
         //     this.setState({  // add new event data
         //         calendarEvents: this.state.calendarEvents.concat({ // creates a new array
@@ -39,11 +86,7 @@ const [calendarEvents, setCalendarEvents] = React.useState([  { title: 'Event No
 
     return (
         <div className='demo-app'>
-            <div className='demo-app-top'>
-                <button onClick={toggleWeekends}>toggle weekends</button>&nbsp;
-          <button onClick={gotoPast}>go to a date in the past</button>&nbsp;
-          (also, click a date/time to add an event)
-        </div>
+
             <div className='demo-app-calendar'>
                 <FullCalendar
                     defaultView="dayGridMonth"
@@ -54,9 +97,9 @@ const [calendarEvents, setCalendarEvents] = React.useState([  { title: 'Event No
                     }}
                     plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
                     ref={calendarComponentRef}
-                    weekends={calendarWeekends}
+                    // weekends={calendarWeekends}
                     events={calendarEvents}
-                    dateClick={handleDateClick}
+                // dateClick={handleDateClick}
                 />
             </div>
         </div>
